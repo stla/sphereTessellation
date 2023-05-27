9e7abcb3d22ad360bc0f9cfc6b76efa43629e0fd
Stéphane Laurent <laurent_step@yahoo.fr>
2023-05-27 16:18:40 +0200

    started spherical triangle

--- src/sphereTessellation.h
+++ src/sphereTessellation.h
@@ -3,22 +3,32 @@
 
 #include <Rcpp.h>
 
+#define CGAL_EIGEN3_ENABLED 1
+
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_on_sphere_2.h>
 #include <CGAL/Projection_on_sphere_traits_3.h>
-//#include <boost/iterator/transform_iterator.hpp>
 #include <CGAL/circulator.h>
 #include <CGAL/Circular_arc_point_3.h>
-#include <CGAL/Triangulation_vertex_base_with_info_2.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel          K;
-typedef CGAL::Projection_on_sphere_traits_3<K>                       Traits;
-typedef CGAL::Delaunay_triangulation_on_sphere_2<Traits>             DToS2;
-typedef Traits::Point_3                                              SPoint3;
-typedef Traits::Arc_on_sphere_2                                      Arc;
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/boost/graph/generators.h>
+#include <CGAL/Polygon_mesh_processing/clip.h>
+#include <CGAL/Plane_3.h>
+#include <CGAL/Subdivision_method_3/subdivision_methods_3.h>
 
-
-typedef CGAL::Container_from_circulator<DToS2::Edge_circulator> CC_Edges;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel       K;
+typedef CGAL::Projection_on_sphere_traits_3<K>                    Traits;
+typedef CGAL::Delaunay_triangulation_on_sphere_2<Traits>          DToS2;
+typedef Traits::Point_3                                           SPoint3;
+typedef Traits::Arc_on_sphere_2                                   Arc;
+typedef CGAL::Container_from_circulator<DToS2::Edge_circulator>   CC_Edges;
 typedef CGAL::Container_from_circulator<DToS2::Vertex_circulator> CC_Vertices;
+
+typedef K::Point_3                                       Point3;
+typedef CGAL::Surface_mesh<Point3>                       Mesh3;
+typedef K::Plane_3                                       Plane3;
+
+namespace PMP = CGAL::Polygon_mesh_processing;
 
 #endif
---
+++ src/sphericalTriangle.cpp
@@ -0,0 +1,26 @@
+#include "sphereTessellation.h"
+
+void clippingToPlane(Mesh3& mesh, Plane3 plane) {
+  const bool clipping = PMP::clip(
+    mesh, plane,
+    PMP::parameters::clip_volume(false)
+  );
+  if(!clipping) {
+    Rcpp::stop("Clipping has failed.");
+  }
+  mesh.collect_garbage();
+}
+
+// -------------------------------------------------------------------------- //
+// -------------------------------------------------------------------------- //
+Mesh3 icosphere(Point3 center, double radius, unsigned int iterations) {
+  // !!! bug CGAL 5.4 !!! //
+  double x = sqrt(1 + (1 + sqrt(5)) / 4);
+  radius = radius / x;
+  Mesh3 mesh;
+  CGAL::make_icosahedron<Mesh3, Point3>(mesh, center, radius);
+  CGAL::Subdivision_method_3::Loop_subdivision(
+    mesh, CGAL::parameters::number_of_iterations(iterations)
+  );
+  return mesh;
+}
